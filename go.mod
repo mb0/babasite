27724fbe9fb8ff7fa5901b05cdadd90dd9e889c4
Martin Schnabel <mb0@mb0.org>
2022-03-23 03:07:09 +0100

    add simple embedded database

--- go.mod
+++ go.mod
@@ -6,5 +6,13 @@ require xelf.org/daql v0.0.0-20220130075634-9a6317987292
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/tidwall/btree v1.1.0 // indirect
+	github.com/tidwall/buntdb v1.2.9
+	github.com/tidwall/gjson v1.12.1 // indirect
+	github.com/tidwall/grect v0.1.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/rtred v0.1.2 // indirect
+	github.com/tidwall/tinyqueue v0.1.1 // indirect
 	xelf.org/xelf v0.0.0-20220126024223-0139d0cd0171 // indirect
 )
--- main.go
+++ main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/mb0/babasite/gol"
 	"github.com/mb0/babasite/maped"
 	"github.com/mb0/babasite/site"
+	"github.com/tidwall/buntdb"
 	"xelf.org/daql/hub/wshub"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http server address")
+var dbpath = flag.String("db", "data.db", "buntdb file path or :memory:")
 
 func main() {
+	// parse the above command line args e.g. "babasite -db=./mydata.db"
 	flag.Parse()
+
+	// open a simple database
+	db, err := buntdb.Open(*dbpath)
+	if err != nil {
+		log.Fatalf("failed to open %s: %v", *dbpath, err)
+	}
+	// close the db when the main function ends
+	defer db.Close()
+
 	s := site.NewSite(
 		site.NewChat("simple"),
 		gol.NewRoom("gol"),
