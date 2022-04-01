0c9d7aaf8bbebd7e212bfca3917a5851811d461c
Martin Schnabel <mb0@mb0.org>
2022-04-01 02:13:59 +0200

    chared: add box and simple int geom helpers

--- chared/edit.go
+++ chared/edit.go
@@ -1,67 +1,82 @@
 package chared
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type EditPic struct {
-	Seq  string `json:"seq"`
-	Pic  int    `json:"pic"`
-	Copy bool   `json:"copy,omitempty"`
-	Sel
-}
+type Pixel uint16
 
 type Sel struct {
-	Pos
-	Size
+	Box
 	Data []Pixel `json:"data"`
 }
 
-type Pos struct {
-	X int `json:"x"`
-	Y int `json:"y"`
+func (s Sel) Global(p Pos) Pixel {
+	if !p.In(s.Box) {
+		return 0
+	}
+	return s.Data[(p.Y-s.Y)*s.W+p.X-s.X]
+}
+
+func MakeSel(x, y, w, h int, pix ...Pixel) Sel {
+	if n := w * h; len(pix) < n {
+		old := pix
+		pix = make([]Pixel, n)
+		copy(pix, old)
+	}
+	return Sel{Box: MakeBox(x, y, w, h), Data: pix}
+}
+
+func (pic *Sel) Draw(e Sel, cp bool) {
+	if e.Empty() {
+		return
+	}
+	if pic.Box == e.Box { // simple case same box
+		if cp {
+			copy(pic.Data, e.Data)
+			return
+		}
+		for i, p := range e.Data {
+			if p != 0 {
+				pic.Data[i] = p
+			}
+		}
+		return
+	}
+	if !pic.Contains(e.Box) { // we need to grow
+		tmp := &Sel{Box: pic.Grow(e.Box)}
+		tmp.Data = make([]Pixel, tmp.W*tmp.H)
+		tmp.Draw(*pic, true)
+		*pic = *tmp
+	}
+	// we can calculate
+	for i, p := range e.Data {
+		if cp || p != 0 {
+			x := i%e.W + e.X
+			y := i/e.W + e.Y
+			pic.Data[(y-pic.Y)*pic.W+x-pic.X] = p
+		}
+	}
+}
+
+type EditPic struct {
+	Pic  int  `json:"pic"`
+	Copy bool `json:"copy,omitempty"`
+	Sel
 }
 
 // Apply changes asset a with the given edit or returns an error.
 func Apply(a *Asset, e EditPic) error {
 	// TODO validate edit
-	if !a.ValidSel(e.Sel) {
+	b := Box{Size: a.Size}
+	if !b.ValidSel(e.Sel.Box) {
 		return fmt.Errorf("edit selection invalid")
 	}
 	// first look up the pic to edit
-	pic := a.GetPic(e.Seq, e.Pic)
+	pic := a.Pics[e.Pic]
 	if pic == nil {
-		return fmt.Errorf("no sequence named %s", e.Seq)
+		return fmt.Errorf("no picture with id %d", e.Pic)
 	}
-	if e.Copy {
-		CopySel(pic, e.Sel)
-	} else {
-		DrawSel(pic, e.Sel)
-	}
+	pic.Sel.Draw(e.Sel, e.Copy)
 	return nil
-}
-
-func CopySel(pic *Pic, e Sel) {
-	// check whether this is a full image edit
-	if len(pic.Data) == len(e.Data) {
-		// same length, we can copy the whole list
-		copy(pic.Data, e.Data)
-	} else {
-		// we edit a pic subset the row segments
-		h := e.Y + e.H
-		for y := e.Y; y < h; y++ {
-			di := (y - e.Y) * e.W
-			copy(pic.Data[y*pic.W+e.X:], e.Data[di:di+e.W])
-		}
-	}
-}
-
-func DrawSel(pic *Pic, e Sel) {
-	for ey := 0; ey < e.H; ey++ {
-		row := (e.Y + ey) * pic.W
-		for ex := 0; ex < e.W; ex++ {
-			v := e.Data[ey*e.W+ex]
-			if v != 0 {
-				pic.Data[row+e.X+ex] = v
-			}
-		}
-	}
 }
--- chared/edit_test.go
+++ chared/edit_test.go
@@ -8,39 +8,33 @@ import (
 func TestCopySel(t *testing.T) {
 	tests := []struct {
 		name string
-		pic  Pic
+		pic  Sel
 		sel  Sel
 		want []Pixel
 	}{
-		{"fill copy", Pic{Size{3, 3}, make([]Pixel, 9)}, Sel{
-			Size: Size{W: 3, H: 3},
-			Data: []Pixel{1, 1, 1, 1, 1, 1, 1, 1, 1},
-		}, []Pixel{
-			1, 1, 1,
-			1, 1, 1,
-			1, 1, 1,
-		}},
-		{"fill part", Pic{Size{3, 3}, make([]Pixel, 9)}, Sel{
-			Pos:  Pos{X: 1, Y: 1},
-			Size: Size{W: 2, H: 2},
-			Data: []Pixel{1, 2, 3, 4},
-		}, []Pixel{
-			0, 0, 0,
-			0, 1, 2,
-			0, 3, 4,
-		}},
-		{"dot end", Pic{Size{3, 3}, make([]Pixel, 9)}, Sel{
-			Pos:  Pos{X: 1, Y: 2},
-			Size: Size{W: 1, H: 1},
-			Data: []Pixel{1},
-		}, []Pixel{
-			0, 0, 0,
-			0, 0, 0,
-			0, 1, 0,
-		}},
+		{"fill copy", MakeSel(0, 0, 3, 3), MakeSel(0, 0, 3, 3,
+			1, 1, 1, 1, 1, 1, 1, 1, 1),
+			[]Pixel{
+				1, 1, 1,
+				1, 1, 1,
+				1, 1, 1,
+			}},
+		{"fill part", MakeSel(0, 0, 3, 3), MakeSel(1, 1, 2, 2,
+			1, 2, 3, 4),
+			[]Pixel{
+				0, 0, 0,
+				0, 1, 2,
+				0, 3, 4,
+			}},
+		{"dot end", MakeSel(0, 0, 3, 3), MakeSel(1, 2, 1, 1, 1),
+			[]Pixel{
+				0, 0, 0,
+				0, 0, 0,
+				0, 1, 0,
+			}},
 	}
 	for _, test := range tests {
-		CopySel(&test.pic, test.sel)
+		test.pic.Draw(test.sel, true)
 		if !reflect.DeepEqual(test.pic.Data, test.want) {
 			t.Errorf("failed test %s\n%v", test.name, test.pic.Data)
 		}
