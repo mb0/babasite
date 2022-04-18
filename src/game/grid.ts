dee25922b9000057a2fd1a1a066d5ad64c1552e8
Martin Schnabel <mb0@mb0.org>
2022-04-18 21:06:58 +0200

    grid: add mirror, transpose and rotate functions

--- src/game/grid.ts
+++ src/game/grid.ts
@@ -46,3 +46,68 @@ export function gridEach<T>(g:Grid<T>, f:EachFunc<T>, b?:Box, not?:T) {
 		}
 	}
 }
+
+export function gridMirrorH<T>(g:Grid<T>) {
+	const hw = Math.floor(g.w/2)
+	for (let y=0; y < g.h; y++) {
+		for (let x=0; x < hw; x++) {
+			const p = {x:g.x+x, y:g.y+y}
+			const o = {x:g.x+g.w-1-x, y:p.y}
+			const t = g.get(p)
+			g.set(p, g.get(o))
+			g.set(o, t)
+		}
+	}
+}
+
+export function gridMirrorV<T>(g:Grid<T>) {
+	const hh = Math.floor(g.h/2)
+	for (let y=0; y < hh; y++) {
+		for (let x=0; x < g.w; x++) {
+			const p = {x:g.x+x, y:g.y+y}
+			const o = {x:p.x, y:g.y+g.h-1-y}
+			const t = g.get(p)
+			g.set(p, g.get(o))
+			g.set(o, t)
+		}
+	}
+}
+
+export function gridTranspose<T>(g:Grid<T>) {
+	const {w,h} = g
+	if (w == h) {
+		for (let y=0; y<h; y++) {
+			for (let x=y+1; x<w; x++) {
+				const p = {x:g.x+x, y:g.y+y}
+				const o = {x:g.x+y, y:g.y+x}
+				const t = g.get(p)
+				g.set(p, g.get(o))
+				g.set(o, t)
+			}
+		}
+		return
+	} else {
+		const n = {...g, w:h, h:w, raw:Array(g.raw.length)}
+		for (let y=0; y<h; y++) {
+			for (let x=0; x<w; x++) {
+				const p = {x:g.x+x, y:g.y+y}
+				const o = {x:g.x+y, y:g.y+x}
+				n.set(o, g.get(p))
+			}
+		}
+		Object.assign(g, n)
+	}
+}
+
+export function gridRot90<T>(g:Grid<T>) {
+	gridMirrorV(g)
+	gridTranspose(g)
+}
+export function gridRot180<T>(g:Grid<T>) {
+	gridMirrorH(g)
+	gridMirrorV(g)
+}
+export function gridRot270<T>(g:Grid<T>) {
+	gridTranspose(g)
+	gridMirrorV(g)
+}
