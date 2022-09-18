321b5c243f1c4a6091a13b08eb2222ab3b5cf8da
Martin Schnabel <mb0@mb0.org>
2022-09-18 21:33:39 +0200

    wedit: add tree view actions for some topics

--- src/wedit/view_world.ts
+++ src/wedit/view_world.ts
@@ -1,13 +1,16 @@
-import h from 'web/html'
+import h, {hIcon} from 'web/html'
 import {Menu} from 'web/menu'
 import {Dock, Layout} from 'game/dock'
 import app from 'app'
 import {Img} from 'game/pix'
 import {Grid} from 'game/lvl'
 import {WorldData, namedTables} from './world'
-import {LvlView} from './view_lvl'
-import {ImgView, PicData} from './view_img'
-import {nameInput, simpleForm} from './form'
+import {lvlForm, LvlView} from './view_lvl'
+import {imgForm, ImgView, PicData} from './view_img'
+import {mount, unmount} from 'web/modal'
+import {tsetForm} from './view_tset'
+import {palForm} from './view_pal'
+import {clipForm} from './view_clip'
 
 export function worldSel(worlds:string[]):HTMLElement {
 	return h('', "Welt auswählen", h('ul', worlds.map(w =>
@@ -56,7 +59,7 @@ export function worldIndex(d:WorldData) {
 	return h('.windex', h('h1', "World "+ d.name))
 }
 
-type TopClick = (top:string, el:any)=>void
+type TopAct = (top:string, el?:any)=>void
 
 class TreeView implements Dock {
 	el = h('ul.wtree')
@@ -64,60 +67,80 @@ class TreeView implements Dock {
 	group = "wedit"
 	cont?:HTMLElement
 	menu:Menu
+	act:TopAct
 	constructor(public d:WorldData) {
 		this.label = "World "+ d.name
-		this.menu = {act:it=>console.log('menu', it), list:[
+		this.act = (top:string, el?:any) => {
+			if (top == "world.sel") {
+				app.rr.go("/wedit")
+			} else if (top.indexOf('.') < 0) {
+				console.log("open "+ top, el)
+				if (top == 'clip') {
+					app.rr.go(`/wedit/${d.name}/img/${el.img}/${el.id}`)
+				} else {
+					app.rr.go(`/wedit/${d.name}/${top}/${el.id}`)
+				}
+			} else {
+				const send = (res:any) => {
+					app.send(top, res)
+					unmount()
+				}
+				const open = (res:any) => {
+					res.open = true
+					send(res)
+				}
+				let f:any = null
+				if (top == "tset.new") f = tsetForm({}, send)
+				else if (top == "lvl.new") f = lvlForm(d, {}, open)
+				else if (top == "pal.new") f = palForm({}, send)
+				else if (top == "img.new") f = imgForm(d, {}, open)
+				else if (top == "clip.new") f = clipForm({img:el.id, w:el.w, h:el.h}, send)
+				if (f) mount(f)
+				else console.log(`action ${top} not implemented`)
+			}
+		}
+		this.menu = {act:this.act, list:[
 			{name:'world.sel', icon:'swap', label:'Welt auswählen'},
 		]}
 		this.update()
 	}
 	update():void {
 		const {d} = this
-		const click = (top:string, el:any) => {
-			console.log("open "+ top, el)
-			if (top == 'clip') {
-				app.rr.go(`/wedit/${d.name}/img/${el.img}/${el.id}`)
-			} else {
-				app.rr.go(`/wedit/${d.name}/${top}/${el.id}`)
-			}
-		}
 		h.repl(this.el, namedTables.map(({top, label}) => {
 			if (top == "clip") return null
-			const sub = top == "img" ? imgTree(d, click) :
-				tableTree((d as any)[top], top, click)
+			const sub = top == "img" ? imgTree(d, this.act) :
+				tableTree((d as any)[top], top, this.act)
 			const open = top == "lvl" || top == "img"
-			return h('li.sum', h('details', {open}, h('summary', label), sub))
+			const ic = h('a', {title:label+' hinzufügen', onclick: (e:Event) => {
+				e.preventDefault()
+				this.act(top+'.new')
+			}}, hIcon('plus', {}))
+			return h('li.sum', h('details', {open}, h('summary', label, ic), sub))
 		}))
 	}
 }
 
-function tableTree(list:any[], top:string, click:TopClick):HTMLElement {
+function tableTree(list:any[], top:string, act:TopAct):HTMLElement {
 	return h('ul', list.map((el:any) => {
-		return h('li', {onclick: () => click(top, el)}, el.name || '(Ohne Namen)')
+		return h('li', {onclick: () => act(top, el)}, el.name || '(Ohne Namen)')
 	}))
 }
 
-function imgTree(d:WorldData, click:TopClick):HTMLElement {
+function imgTree(d:WorldData, act:TopAct):HTMLElement {
 	return h('ul', d.img.fmap((img:Img) => {
 		const clips = d.clip.all(c => c.img == img.id)
 		const onctx = (e:any) => {
 			e.preventDefault()
-			click("img", img)
+			act("img", img)
 		}
-		return h('li.sum',
-			h('details', h('summary', {oncontextmenu: onctx}, img.name || '(Ohne Namen)'),
-				tableTree(clips, "clip", click)
-			),
-		)
+		const ic = h('a', {title:'Clip hinzufügen', onclick: (e:Event) => {
+			e.preventDefault()
+			act('clip.new', img)
+		}}, hIcon('plus', {}))
+		return h('li.sum', h('details',
+			h('summary', {oncontextmenu: onctx}, img.name || '(Ohne Namen)', ic),
+			tableTree(clips, "clip", act)
+		))
 	}))
 }
 
-interface WorldInfo {
-	name:string
-}
-
-export function worldForm(s:Partial<WorldInfo>, isnew:boolean, submit:(res:Partial<WorldInfo>)=>void) {
-	return simpleForm<WorldInfo>('World', s, isnew, submit, [
-		nameInput(s.name),
-	])
-}
